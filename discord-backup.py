e76415028f0680ff086134232c73a66ade51f909
Dominic Prior <dominicprior42@gmail.com>
2023-08-10 09:53:27 +0100

    Add thread messages

--- discord-backup.py
+++ discord-backup.py
@@ -1,5 +1,7 @@
 # Writes a backup of a discord server as a json string.
 # Inspired by https://www.youtube.com/watch?v=xh28F6f-Cds
+# This was useful too:
+# https://gist.github.com/hackermondev/5c928ca12b4f4e6320100b11f798c23b
 # It is currently hardwired to backup the "AI chat" server.
 # The auth argument can be found by examining the network
 # traffic in the browser devtools as shown in the YouTube video.
@@ -11,17 +13,23 @@ parser = argparse.ArgumentParser(description='Prints the channels and messages')
 parser.add_argument('auth')
 args = parser.parse_args()
 headers = { 'authorization': args.auth }
-serverId = '1116612173767135342'  # AI chat server
-r = requests.get(f'https://discord.com/api/v9/guilds/{serverId}/channels',
-        headers=headers)
-if r.status_code != 200:
-    sys.exit(r.text)
-channels = json.loads(r.text)
-messages = []
+serverId = '1116612173767135342'  # AI chat
+
+def get(url):
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        sys.exit(r.text)
+    return json.loads(r.text)
+
+channels = get(f'https://discord.com/api/v9/guilds/{serverId}/channels')
+allMessages = []
 for channel in channels:
-    channelId = channel['id']
-    r = requests.get(f'https://discord.com/api/v9/channels/{channelId}/messages',
-                    headers=headers)
-    m = json.loads(r.text)
-    messages.append({'channel': channel, 'messages': m})
-print(json.dumps(messages, indent=4))
+    messages = get(f'https://discord.com/api/v9/channels/{channel["id"]}/messages')
+    for message in messages:
+        message['threadMessages'] = []
+        if 'thread' in message:
+            threadId = message['thread']['id']
+            threadMessages = get(f'https://discord.com/api/v9/channels/{threadId}/messages')
+            message['threadMessages'] += [t for t in threadMessages if t['content'] != '']
+    allMessages.append({'channel': channel, 'messages': messages})
+print(json.dumps(allMessages, indent=4))
